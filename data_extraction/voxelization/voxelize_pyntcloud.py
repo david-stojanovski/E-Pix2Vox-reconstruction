e4fe52bb46dd9b5afd882e8f1faa78626608c96b
david_stojanovski <07dstojanovski@gmail.com>
2022-07-29 13:59:42 +0100

    bug fix in imports

--- data_extraction/voxelization/voxelize_pyntcloud.py
+++ data_extraction/voxelization/voxelize_pyntcloud.py
@@ -6,8 +6,8 @@ import pandas as pd
 from natsort import natsorted
 from pyntcloud import PyntCloud
 
-from slicing.config import cfg
-from slicing.plane_extract import prepare_meshes
+from data_extraction.slicing.config import cfg
+from data_extraction.slicing.plane_extract import prepare_meshes
 
 
 def convert_vtk2np_voxels(in_mesh, vol_resolution):
