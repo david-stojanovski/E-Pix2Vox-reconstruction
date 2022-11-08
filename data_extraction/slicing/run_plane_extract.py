9afc4296cf8a13fe1717b83430274e20249b4244
david_stojanovski <07dstojanovski@gmail.com>
2022-11-08 17:45:13 +0000

    Add multiprocessing support and small bugfix
    Added ability to use multiprocessing with a user defined value of cpu cores in the config file. Also relocated the previously defined files paths to the config file

--- data_extraction/slicing/run_plane_extract.py
+++ data_extraction/slicing/run_plane_extract.py
@@ -5,25 +5,32 @@ from natsort import natsorted
 
 from config import cfg
 from plane_extract import run_slice_extraction
+from multiprocessing import Pool, process
+from itertools import repeat
+import time
 
 
 def main():
-    # Change these values
-    data_folder = '/folder/containing/binary/meshes/'
-    save_folder = r'/folder/to/save/slices/to'
-    #
+    all_data_paths = natsorted(glob.glob(cfg.DATA_IN.DATA_FOLDER + '*.vtk'))
+    start = time.perf_counter()
+    case_names = [file.split(os.sep)[-1].split('.')[0] for file in all_data_paths]
 
-    all_data_paths = natsorted(glob.glob(data_folder + '*.vtk'))
-    for file in all_data_paths:
-        case_name = file.split(os.sep)[-1].split('.')[0]
-        save_path = os.path.join(save_folder, 'heart_seg', 'heart_render', 'heart', case_name)
+    save_paths = [os.path.join(cfg.DATA_IN.SAVE_FOLDER, 'heart_seg', 'heart_render', 'heart', case_name) for case_name
+                  in
+                  case_names]
 
-        if not os.path.isdir(save_path):
-            os.makedirs(save_path)
+    for path in save_paths:
+        if not os.path.exists(path):
+            os.makedirs(path)
 
-        run_slice_extraction(cfg, file, save_path, show_fancyplot=False)
-        print('Finished file:', file)
+    with Pool(cfg.PARAMETERS.NUM_WORKERS) as pool:
+        pool.starmap(run_slice_extraction,
+                     zip(repeat(cfg), all_data_paths, save_paths, repeat(cfg.DATA_OUT.FANCY_PLOT)))
+
+    finish = time.perf_counter()
+    print(f'Finished in {finish - start} second(s)')
 
 
 if __name__ == '__main__':
     main()
+
