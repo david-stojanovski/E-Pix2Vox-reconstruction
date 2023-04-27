4c14633dd46a9b257444579305ea8f7a2b7206e9
david_stojanovski <07dstojanovski@gmail.com>
2023-04-27 10:39:39 +0100

    remove deprecated numpy datatypes
    removed the numpy np.int and np.bool datatypes as they have become deprecated for built in python datatypes.

--- e_pivox/utils/binvox_rw.py
+++ e_pivox/utils/binvox_rw.py
@@ -176,7 +176,7 @@ def read_as_coord_array(fp, fix_coords=True):
     end_indices = np.cumsum(counts)
     indices = np.concatenate(([0], end_indices[:-1])).astype(end_indices.dtype)
 
-    values = values.astype(np.bool)
+    values = values.astype(bool)
     indices = indices[values]
     end_indices = end_indices[values]
 
@@ -203,7 +203,7 @@ def read_as_coord_array(fp, fix_coords=True):
     return Voxels(np.ascontiguousarray(data), dims, translate, scale, axis_order)
 
 
-def dense_to_sparse(voxel_data, dtype=np.int):
+def dense_to_sparse(voxel_data, dtype=int):
     """ From dense representation to sparse (coordinate) representation.
     No coordinate reordering.
     """
@@ -212,14 +212,14 @@ def dense_to_sparse(voxel_data, dtype=np.int):
     return np.asarray(np.nonzero(voxel_data), dtype)
 
 
-def sparse_to_dense(voxel_data, dims, dtype=np.bool):
+def sparse_to_dense(voxel_data, dims, dtype=bool):
     if voxel_data.ndim != 2 or voxel_data.shape[0] != 3:
         raise ValueError('[ERROR] voxel_data is wrong shape; should be 3xN array.')
     if np.isscalar(dims):
         dims = [dims] * 3
     dims = np.atleast_2d(dims).T
     # truncate to integers
-    xyz = voxel_data.astype(np.int)
+    xyz = voxel_data.astype(int)
     # discard voxels that fall outside dims
     valid_ix = ~np.any((xyz < 0) | (xyz >= dims), 0)
     xyz = xyz[:, valid_ix]
